9b0c0bd0b79ed3476dae683d97d42c29fff1fe94
Yi Kanghoon <hoon_5083@naver.com>
2022-07-08 07:20:38 +0900

    refact

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "hooks/useUser";
 import useSWR from "swr";
 import { User } from "types/api";
 import fetcher from "utils/fetcher";
@@ -5,10 +6,7 @@ import Detail from "./Detail";
 
 function Profile() {
 	//데이터 가져오기
-	const { data, error } = useSWR<User>(
-		"https://reqres.in/api/users/2",
-		fetcher
-	);
+	const { data } = useUser(2);
 	return (
 		<div className="overflow-hidden bg-white shadow-xl rounded-3xl">
 			<div className="p-6 bg-blue-500 pb-14">
---
+++ src/hooks/useUser.ts
@@ -0,0 +1,7 @@
+import useSWR from "swr";
+import { User } from "types/api";
+import fetcher from "utils/fetcher";
+
+export function useUser(id: number) {
+	return useSWR<User>(`https://reqres.in/api/users/${id}`, fetcher);
+}
